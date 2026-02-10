58b86074ed7ea2f189650398eb5e1c8edeef3a7b
Bulat <babdullatypov@gmail.com>
2026-02-10 19:05:36 +0300

    Add ability to specify web server port via env var

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 var port = 7540
 var webDir = "./web"
 
+func getPort(defaultPort int) int {
+	port := defaultPort
+	envPort := os.Getenv("TODO_PORT")
+	if len(envPort) > 0 {
+		if parsedPort, err := strconv.ParseInt(envPort, 10, 32); err == nil {
+			port = int(parsedPort)
+		}
+	}
+	return port
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort(port)), nil)
 	if err != nil {
 		panic(err)
 	}
