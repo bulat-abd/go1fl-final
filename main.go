ef6fac230869c84c47332c9302d07e69e6ad90e7
Bulat <babdullatypov@gmail.com>
2026-02-10 19:30:43 +0300

    Factor out web server functionality to separate package

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
+	"github.com/bulat-abd/go1fl-final/pkg/server"
 )
 
-var port = 7540
-var webDir = "./web"
-
-func getPort(defaultPort int) int {
-	port := defaultPort
-	envPort := os.Getenv("TODO_PORT")
-	if len(envPort) > 0 {
-		if parsedPort, err := strconv.ParseInt(envPort, 10, 32); err == nil {
-			port = int(parsedPort)
-		}
-	}
-	return port
-}
-
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort(port)), nil)
-	if err != nil {
-		panic(err)
-	}
+	server.Run()
 }
---
+++ pkg/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var port = 7540
+var webDir = "./web"
+
+func getPort(defaultPort int) int {
+	port := defaultPort
+	envPort := os.Getenv("TODO_PORT")
+	if len(envPort) > 0 {
+		if parsedPort, err := strconv.ParseInt(envPort, 10, 32); err == nil {
+			port = int(parsedPort)
+		}
+	}
+	return port
+}
+
+func Run() {
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort(port)), nil)
+	if err != nil {
+		panic(err)
+	}
+}
