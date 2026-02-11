845b271fdf77c1c8f107e68c7ecc384c14f32f4a
Bulat <babdullatypov@gmail.com>
2026-02-11 04:08:25 +0300

    Add basic code to satisfy tests

---
+++ pkg/db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "modernc.org/sqlite"
+)
+
+var FilePath = "scheduler.db"
+
+func Init(dbFile string) (*sql.DB, error) {
+	_, err := os.Stat(dbFile)
+	var install bool
+	if err != nil {
+		install = true
+	}
+	db, err := sql.Open("sqlite", dbFile)
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS "scheduler"
+	(
+	    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date CHAR(8) NOT NULL DEFAULT "",
+    	title varchar(128) NOT NULL,
+        comment text NOT NULL,
+        repeat varchar(128) NOT NULL
+	);`
+	if install {
+		_, err := db.Exec(createTableSQL)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+	}
+	return db, nil
+}
