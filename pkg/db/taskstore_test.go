a6fd7638e7c0815d542280cebe40dfb7535ba8e6
Bulat <babdullatypov@gmail.com>
2026-02-12 02:41:26 +0300

    Add tests

--- pkg/db/taskstore_test.go
+++ pkg/db/taskstore_test.go
@@ -53,3 +53,31 @@ func TestAddGetDelete(t *testing.T) {
 	_, err = store.Get(id)
 	require.Error(t, err)
 }
+
+func TestSetDate(t *testing.T) {
+	db, err := sql.Open("sqlite", "file:test.db?mode=memory&cache=shared")
+	require.NoError(t, err)
+	defer db.Close()
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS "scheduler"
+	(
+	    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date CHAR(8) NOT NULL DEFAULT "",
+    	title varchar(128) NOT NULL,
+        comment text NOT NULL,
+        repeat varchar(128) NOT NULL
+	);`
+	_, err = db.Exec(createTableSQL)
+	require.NoError(t, err)
+	store := NewTaskStore(db)
+	task := getTestTask()
+
+	id, err := store.Add(task)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	err = store.SetDate(id, "13372048")
+	storedTask, err := store.Get(id)
+	require.NoError(t, err)
+	assert.Equal(t, storedTask.Date, "13372048")
+}
