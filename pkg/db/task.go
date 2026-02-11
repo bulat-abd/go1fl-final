09cdf0ba00f146811ad465dd63d030806a08fbf6
Bulat <babdullatypov@gmail.com>
2026-02-12 02:29:30 +0300

    Implement CRD of CRUD

--- pkg/db/task.go
+++ pkg/db/task.go
@@ -1,7 +1,5 @@
 package db
 
-import "database/sql"
-
 type Task struct {
 	ID      int64  `db:"id"`
 	Date    string `db:"date"`
@@ -16,12 +14,4 @@ type TaskService struct {
 
 func NewTaskService(store TaskStore) TaskService {
 	return TaskService{store: store}
-}
-
-type TaskStore struct {
-	db *sql.DB
-}
-
-func NewTaskStore(db *sql.DB) TaskStore {
-	return TaskStore{db: db}
 }
---
+++ pkg/db/taskstore.go
@@ -0,0 +1,44 @@
+package db
+
+import "database/sql"
+
+type TaskStore struct {
+	db *sql.DB
+}
+
+func NewTaskStore(db *sql.DB) TaskStore {
+	return TaskStore{db: db}
+}
+
+func (ts TaskStore) Add(t Task) (int64, error) {
+	res, err := ts.db.Exec(
+		"INSERT INTO scheduler (date, title, comment, repeat) VALUES (:date, :title, :comment, :repeat)",
+		sql.Named("date", t.Date),
+		sql.Named("title", t.Title),
+		sql.Named("comment", t.Comment),
+		sql.Named("repeat", t.Repeat),
+	)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	return id, err
+}
+
+func (ts TaskStore) Get(id int64) (Task, error) {
+	row := ts.db.QueryRow("SELECT id, date, title, comment, repeat FROM scheduler WHERE id = :id", sql.Named("id", id))
+	t := Task{}
+	err := row.Scan(&t.ID, &t.Date, &t.Title, &t.Comment, &t.Repeat)
+	if err != nil {
+		return Task{}, err
+	}
+	return t, nil
+}
+
+func (ts TaskStore) Delete(id int64) error {
+	_, err := ts.db.Exec(
+		"DELETE FROM scheduler WHERE id = :id",
+		sql.Named("id", id),
+	)
+	return err
+}
