cd14cd2a2cb24d841e0f8cd1706470de3adb8e72
Bulat <babdullatypov@gmail.com>
2026-02-22 01:28:42 +0300

    Implement w repeat parameter

--- pkg/api/nextdate.go
+++ pkg/api/nextdate.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"net/http"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -17,7 +18,6 @@ func NextDate(now time.Time, dstart string, repeat string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	if string(repeat[0]) == "d" {
 		parts := strings.Split(repeat, " ")
 		if len(parts) < 2 {
@@ -42,6 +42,37 @@ func NextDate(now time.Time, dstart string, repeat string) (string, error) {
 		for {
 			date = date.AddDate(1, 0, 0)
 			if afterNow(date, now) {
+				break
+			}
+		}
+		return date.Format("20060102"), nil
+	} else if string(repeat[0]) == "w" {
+		wstr := strings.Split(repeat, " ")
+		if len(wstr) != 2 {
+			return "", errors.New("Bad repeat parameter value")
+		}
+		daystr := strings.Split(wstr[1], ",")
+		if len(daystr) == 0 {
+			return "", errors.New("Bad repeat parameter value")
+		}
+		days := make([]int, 0, len(daystr))
+		for _, dstr := range daystr {
+			day, err := strconv.Atoi(dstr)
+			if err != nil {
+				return "", errors.New("Bad repeat parameter value")
+			}
+			if day < 1 || day > 7 {
+				return "", errors.New("Bad repeat parameter value")
+			}
+			if day == 7 {
+				day = 0
+			}
+			days = append(days, day)
+		}
+		for {
+			date = date.AddDate(0, 0, 1)
+			dayInt := int(date.Weekday())
+			if afterNow(date, now) && slices.Contains(days, dayInt) {
 				break
 			}
 		}
