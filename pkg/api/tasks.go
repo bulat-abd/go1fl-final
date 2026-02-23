374bd02b73403300ec87a7b4a02888c2aac5dc3e
Bulat <babdullatypov@gmail.com>
2026-02-23 20:32:26 +0300

    Implement handler logic for date- and text-based search

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/bulat-abd/go1fl-final/pkg/db"
 )
@@ -16,8 +17,20 @@ type TasksResp struct {
 
 func listTasksHandler(database *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var tasks []db.Task
+		var err error
 		ts := db.NewTaskStore(database)
-		tasks, err := ts.Upcoming(50)
+		search := r.URL.Query().Get("search")
+		if search == "" {
+			tasks, err = ts.Upcoming(50)
+		} else {
+			t, err := time.Parse("02.01.2006", search)
+			if isText := err != nil; isText {
+				tasks, err = ts.SearchByText(search, 50)
+			} else {
+				tasks, err = ts.SearchByDate(t.Format("20060102"), 50)
+			}
+		}
 		if err != nil {
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(map[string]interface{}{
