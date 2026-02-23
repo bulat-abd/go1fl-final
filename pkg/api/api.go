755db6d6594c9830caab769522efdc63199b4705
Bulat <babdullatypov@gmail.com>
2026-02-23 17:26:26 +0300

    DI DB instance into router

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -7,15 +7,15 @@ import (
 
 func Init(db *sql.DB) {
 	http.HandleFunc("/api/nextdate", nextDateHandler)
-	http.HandleFunc("/api/task", addTaskHandler(db))
+	http.HandleFunc("/api/task", taskHandler(db))
 }
 
-/*
-func taskHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	// обработка других методов будет добавлена на следующих шагах
-	case http.MethodPost:
-		addTaskHandler(w, r)
+func taskHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		// обработка других методов будет добавлена на следующих шагах
+		case http.MethodPost:
+			addTaskHandler(db)(w, r)
+		}
 	}
 }
-*/
