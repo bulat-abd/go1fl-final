3e3f893ce7a3b6f27bd96c0ae57247d30ec977ef
Bulat <babdullatypov@gmail.com>
2026-02-25 19:06:20 +0300

    Update test config to use actual token

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `STUBTOKEN`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiMTIzNDUifQ.txTvHeSE4PcjskalN3src5JYiZz7AHnKmYcNh1EKuwQ`
