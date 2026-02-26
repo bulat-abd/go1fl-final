83eb9beadcf7b71298c86fbd00b0354d8b4a3720
Bulat <babdullatypov@gmail.com>
2026-02-26 07:29:38 +0300

    Refactor listTasksHandler to use switch instead of multiple ifs

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -22,16 +22,16 @@ func listTasksHandler(database *sql.DB) func(w http.ResponseWriter, r *http.Requ
 		var err error
 		ts := db.NewTaskStore(database)
 		search := r.URL.Query().Get("search")
-		if search == "" {
+		t, err := time.Parse("02.01.2006", search)
+		switch {
+		case search == "":
 			tasks, err = ts.Upcoming(MaxTasks)
-		} else {
-			t, err := time.Parse("02.01.2006", search)
-			if isText := err != nil; isText {
-				tasks, err = ts.SearchByText(search, MaxTasks)
-			} else {
-				tasks, err = ts.SearchByDate(t.Format("20060102"), MaxTasks)
-			}
+		case err != nil:
+			tasks, err = ts.SearchByText(search, MaxTasks)
+		default:
+			tasks, err = ts.SearchByDate(t.Format("20060102"), MaxTasks)
 		}
+
 		if err != nil {
 			JsonError(w, err.Error(), http.StatusBadRequest)
 			return
