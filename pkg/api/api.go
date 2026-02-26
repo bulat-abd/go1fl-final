0e10b93b753a68146f63f81ddd6fd045294a8bec
Bulat <babdullatypov@gmail.com>
2026-02-26 06:06:16 +0300

    Factor out auth decorator to separate file

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,8 +3,6 @@ package api
 import (
 	"database/sql"
 	"net/http"
-
-	"github.com/bulat-abd/go1fl-final/pkg/token"
 )
 
 func Init(db *sql.DB) {
@@ -39,21 +37,4 @@ func tasksHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 			listTasksHandler(db)(w, r)
 		}
 	}
-}
-
-func auth(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var jwt string
-		cookie, err := r.Cookie("token")
-		if err == nil {
-			jwt = cookie.Value
-		}
-		var valid bool
-		valid = token.ValidateToken(jwt)
-		if !valid {
-			http.Error(w, "Authentification required", http.StatusUnauthorized)
-			return
-		}
-		next(w, r)
-	})
 }
